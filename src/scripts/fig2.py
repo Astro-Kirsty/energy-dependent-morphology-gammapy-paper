d0ca4944f7ab369906cb02d88e84ac697bbb757b
Astro-Kirsty <AstroKirsty@gmail.com>
2026-02-06 18:03:54 +0100

    Adapt fig folder
    Signed-off-by: Astro-Kirsty <AstroKirsty@gmail.com>

--- src/scripts/fig2.py
+++ src/scripts/fig2.py
@@ -6,7 +6,8 @@ from astropy import units as u
 import matplotlib.pyplot as plt
 import paths
 
-significance_cutout = Map.read(paths.data / 'significance_map_J1825.fits')
+#significance_cutout = Map.read(paths.data / 'significance_map_J1825.fits')
+significance_cutout = Map.read('../data/significance_map_J1825.fits')
 PSR_position = SkyCoord(l=18.00018*u.deg, b=-0.69093*u.deg, frame='galactic')
 
 def add_psr(ax, color='mediumturquoise'):
